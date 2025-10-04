3521e682d9aa644bdd25137787c1812b3ecac201
[dyad] <edgar.tavares2026@gmail.com>
2025-10-04 02:41:40 -0300

    [dyad] Atualizando ícones de redes sociais no rodapé - wrote 2 file(s)

--- components/icons.tsx
+++ components/icons.tsx
@@ -76,3 +76,11 @@ export const XIcon: React.FC<IconProps> = (props) => (
     <path d="m6 6 12 12" />
   </svg>
 );
+
+export const TikTokIcon: React.FC<IconProps> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <path d="M9 12v6a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2z" />
+    <path d="M12 12V6a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2z" />
+    <path d="M12 12h-3a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h3" />
+  </svg>
+);
