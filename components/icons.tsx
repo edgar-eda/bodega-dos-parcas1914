8bd28837e7f807dbef7f2346090237595235f459
[dyad] <edgar.tavares2026@gmail.com>
2025-10-04 02:44:55 -0300

    [dyad] Atualizando o ícone do TikTok - wrote 1 file(s)

--- components/icons.tsx
+++ components/icons.tsx
@@ -78,9 +78,7 @@ export const XIcon: React.FC<IconProps> = (props) => (
 );
 
 export const TikTokIcon: React.FC<IconProps> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <path d="M9 12v6a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2z" />
-    <path d="M12 12V6a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2z" />
-    <path d="M12 12h-3a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h3" />
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16" {...props}>
+    <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
   </svg>
 );
