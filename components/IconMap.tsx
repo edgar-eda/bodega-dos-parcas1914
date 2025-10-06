009290e70a07cac8b692b5234a59ac0a80d851b6
[dyad] <edgar.tavares2026@gmail.com>
2025-10-06 19:58:11 -0300

    [dyad] Applying icon updates to the correct file path - wrote 1 file(s) + extra files edited outside of Dyad

--- components/IconMap.tsx
+++ components/IconMap.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import {
-  Beer, CupSoda, GlassWater, Zap, PackagePlus, Wine, Pizza, Sandwich, IceCream, CakeSlice, ShoppingBasket, UtensilsCrossed
+  Beer, CupSoda, GlassWater, Zap, PackagePlus, Wine, Pizza, Sandwich, IceCream, CakeSlice, ShoppingBasket, UtensilsCrossed,
+  Martini, Coffee, Cigarette, Cookie, Beef, Salad, Soup, Candy, ShoppingBag
 } from 'lucide-react';
 
 export const iconMap: { [key: string]: React.ElementType } = {
-  Beer: Beer,
-  CupSoda: CupSoda,
-  GlassWater: GlassWater,
-  Zap: Zap,
-  PackagePlus: PackagePlus,
-  Wine: Wine,
+  Cerveja: Beer,
+  Refrigerante: CupSoda,
+  Agua: GlassWater,
+  Energetico: Zap,
+  Vinho: Wine,
+  Drink: Martini,
+  Cafe: Coffee,
+  Lanche: Sandwich,
   Pizza: Pizza,
-  Sandwich: Sandwich,
-  IceCream: IceCream,
-  CakeSlice: CakeSlice,
-  ShoppingBasket: ShoppingBasket,
-  UtensilsCrossed: UtensilsCrossed,
+  Refeicao: UtensilsCrossed,
+  Churrasco: Beef,
+  Salada: Salad,
+  Caldo: Soup,
+  Sorvete: IceCream,
+  Sobremesa: CakeSlice,
+  Doce: Candy,
+  Snack: Cookie,
+  Tabacaria: Cigarette,
+  Conveniencia: PackagePlus,
+  Cesta: ShoppingBasket,
+  Kit: ShoppingBag,
 };
 
-export const iconList = Object.keys(iconMap);
+export const iconList = Object.keys(iconMap).sort();
 
 export const IconComponent = ({ name, ...props }: { name: string, [key: string]: any }) => {
   const Icon = iconMap[name];
